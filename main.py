bf2b1a23616b2112b73112c0dec6786ceec0a9ca
David Lassiter <david.gray.lassiter@gmail.com>
2024-02-19 23:00:29 +0100

    First user inputs added

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from src.hand import Hand
 
 def main() -> None:
     hand = Hand()
-    logger.info(f"Pot size = {hand.pot_size}")
+    user_input = input("Guess the pot size: ")
+    # TODO: Modify logging to console for user inputs to be color coded and more visible
+    if user_input.isdigit() and int(user_input) == hand.pot_size:
+        logger.info("Correct!")
+    else:
+        logger.info(f"WRONG! The pot size is {hand.pot_size}")
 
 
 if __name__ == "__main__":
