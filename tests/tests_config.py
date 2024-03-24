9de4d3e42c643b5470b64725dfeca0220fcd5d60
David Lassiter <david.gray.lassiter@gmail.com>
2024-03-24 16:29:43 +0100

    Relocate CARDS_DICT

--- tests/tests_config.py
+++ tests/tests_config.py
@@ -1,18 +1,9 @@
-from src.card import Card
+from src.card import CARDS_DICT
 from src.community_cards import CommunityCards
-from src.config import VALID_RANKS_DICT, VALID_SUITS, logger
+from src.config import logger
 from src.deck import Deck
 from src.hole_cards import HoleCards
 from src.player_hand import PlayerHand
-from src.rank import Rank
-from src.suit import Suit
-
-CARDS_DICT = {}
-
-for suit in VALID_SUITS:
-    for rank in VALID_RANKS_DICT.keys():
-        card_name = f"{rank.upper()}_OF_{suit.upper()}"
-        CARDS_DICT[card_name] = Card(Suit(suit), Rank(rank))
 
 HOLE_CARDS_2_3_SPADES = HoleCards(
     deck=Deck(), card1=CARDS_DICT["2_OF_SPADES"], card2=CARDS_DICT["3_OF_SPADES"]
