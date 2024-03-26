7f679b0e26f61ca9756df71133522936871bc4d9
David Lassiter <david.gray.lassiter@gmail.com>
2024-03-26 16:23:25 +0100

    Add tests for hi_card

--- tests/test_hi_card.py
+++ tests/test_hi_card.py
@@ -1,5 +1,19 @@
+from src.config import logger
 from src.hi_card import HI_CARD_HAND_TYPE_SCORE
-from tests.tests_config import hand_type_test_builder, make_community_cards_for_testing
+from src.player_hand import (
+    assert_tie_regardless_of_order,
+    assert_winner_regardless_of_order,
+)
+from tests.test_straight_flush import (
+    HOLE_CARDS_2_7_SPADES_DIAMONDS,
+    HOLE_CARDS_KING_9_CLUBS,
+)
+from tests.tests_config import (
+    HOLE_CARDS_2_3_SPADES,
+    hand_type_test_builder,
+    make_community_cards_for_testing,
+    make_hole_cards_for_testing,
+)
 
 HI_CARD_9 = make_community_cards_for_testing(
     [
@@ -48,6 +62,23 @@ VALID_HI_CARD_CASES_IN_ASCENDING_ORDER = [
     HI_CARD_10_9,
 ]
 
+COMMUNITY_HI_CARD_ACE = make_community_cards_for_testing(
+    [
+        "4_OF_SPADES",
+        "5_OF_CLUBS",
+        "8_OF_HEARTS",
+        "10_OF_HEARTS",
+        "ACE_OF_SPADES",
+    ]
+)
+
+HOLE_CARDS_2_3_CLUBS = make_hole_cards_for_testing(
+    [
+        "2_OF_CLUBS",
+        "3_OF_CLUBS",
+    ]
+)
+
 
 def test_hi_card():
     hand_type_test_builder(
@@ -56,4 +87,22 @@ def test_hi_card():
         expected_hand_type_score=HI_CARD_HAND_TYPE_SCORE,
         valid_tie_case_1=HI_CARD_10_8,
         valid_tie_case_2=HI_CARD_10_8_ALTERNATE,
+    )
+
+
+def test_compare_hi_cards():
+    logger.debug("Test that the stronger hi card hand is always the winner")
+    assert_winner_regardless_of_order(
+        community_cards=COMMUNITY_HI_CARD_ACE,
+        winning_hole_cards=HOLE_CARDS_KING_9_CLUBS,
+        losing_hole_cards=HOLE_CARDS_2_7_SPADES_DIAMONDS,
+    )
+
+
+def test_community_hi_card_ties():
+    logger.debug("Test that a community hi card hand is always a tie")
+    assert_tie_regardless_of_order(
+        community_cards=COMMUNITY_HI_CARD_ACE,
+        hole_cards_1=HOLE_CARDS_2_3_SPADES,
+        hole_cards_2=HOLE_CARDS_2_3_CLUBS,
     )
